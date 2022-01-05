13188ff20f5671fea92dc940edcf644788b4ca2b
ELCI-Linux <88078492+ELCI-Linux@users.noreply.github.com>
2022-01-05 16:04:11 +0000

    Add files via upload
    1.1 - Gnome Network Manager Added via apt (apt-get)
    [New variable $gnm]

--- Waifi.sh
+++ Waifi.sh
@@ -1,24 +1,31 @@
 #! /bin/bash/
 
 helper='Waifi'
-versnum='1.0'
+versnum='1.1'
 
 	waifi=$(zenity --list --checklist \
 		--title="$helper $versnum" \
-		--width=350 \
+		--height=180 --width=500 \
 		--text="Please select the network scanner(s) you would like to install:" \
 		--column="Selected" 	--column="Applications" 	--column="License"	--column="Method" \
-				""		"Linssid"		"GNU Public License V3"		"apt")
+				""		"Linssid"		"GNU Public License V3"		"apt" \
+				""		"Gnome Network Manager"	"GNU Public License V3"		"apt")
 
+
+	gnm=$(echo $waifi | grep -c "Gnome Network Manager")
 	lins=$(echo $waifi | grep -c "Linssid")
 
 
 
 
 
+	if [ $gnm -gt '0' ]; then
+	sudo apt-get install network-manager -y
+	fi
 
 	if [ $lins -gt '0' ]; then
 	sudo add-apt-repository ppa:wseverin/ppa -y
 	sudo apt-get update -y
 	sudo apt-get install linssid -y
 	fi
+
